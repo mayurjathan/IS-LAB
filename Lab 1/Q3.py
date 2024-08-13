54a4c3c2f56c1c54acee2ff6f24c93e43c4e5e39
mayurjathan <mayurjathan999@gmail.com>
2024-08-13 23:46:49 +0530

    Add : Q3 & Q4

---
+++ Lab 1/Q3.py
@@ -0,0 +1,68 @@
+import string 
+
+def generate_playfair_matrix(key):
+    key=''.join(sorted(set(key),key=key.index))
+    alphabet="ABCDEFGHIKLMNOPQRSTUVWXYZ"
+    matrix=[]
+    for char in key.upper():
+        if char not in matrix and char in alphabet:
+            matrix.append(char)
+    for char in alphabet:
+        if char not in matrix:
+            matrix.append(char)
+
+    return [matrix[i:i+5] for i in range(0,25,5)]
+
+def preprocess_message(message):
+    message=''.join(filter(str.isalpha,message)).upper()
+    message=message.replace('J','I')
+    digraphs=[]
+    i=0
+    while i < len(message):
+        a=message[i]
+        b=message[i+1] if i+1 <len(message) else 'X'
+        if(a==b):
+            digraphs.append(a + 'X')
+            i+=1
+        else:
+            digraphs.append(a + b)
+            i+=2
+
+    if len(digraphs[-1]) == 1:
+        digraphs[-1] += 'X'
+
+    return digraphs
+
+def find_position(char,matrix):
+    for i, row in enumerate(matrix):
+        if char in row:
+            return i, row.index(char)
+    return None
+
+def playfair_encrypt(message,matrix):
+     digraphs=preprocess_message(message)
+     encrypted_message=[]
+     for digraph in digraphs:
+         a, b =digraph
+         row_a,col_a=find_position(a,matrix)
+         row_b,col_b=find_position(b,matrix)
+         if row_a==row_b:
+             encrypted_message.append(matrix[row_a][(col_a+1)%5])
+             encrypted_message.append(matrix[row_b][(col_b+1)%5])
+         elif col_a == col_b:
+             encrypted_message.append(matrix[(row_a+1)%5][col_a])
+             encrypted_message.append(matrix[(row_b+1)%5][col_b])
+         else:
+             encrypted_message.append(matrix[row_a][col_b])
+             encrypted_message.append(matrix[row_b][col_a])
+     return ''.join(encrypted_message)
+key=input("Enter the key : ")
+message=input("Enter the key to be encrypted : ")
+matrix=generate_playfair_matrix(key)
+encrypted_message=playfair_encrypt(message,matrix)
+print("Playfair Matrix : ")
+for row in matrix:
+    print(' '.join(row))
+print("\nEncrypted Message: ")
+print(encrypted_message)
+
---
+++ Lab 1/Q4.py
@@ -0,0 +1,39 @@
+import math
+def get_key_matrix(key):
+  size=int(math.sqrt(len(key)))
+  key_matrix=[[0] * size for _ in range(size)]
+  k=0 
+  for i in range(size):
+    for j in range(size):
+      key_matrix[i][j]=key[k]
+      k+=1
+  return key_matrix
+
+def matrix_mult(A,B):
+  size=len(A)
+  result =[[0] * len(B[0]) for _ in range(size)]
+  for i in range(size):
+    for j in range(len(B[0])):
+      for k in range(size):
+        result[i][j] += A[i][k] * B[k][j]
+      result[i][j]%=26
+  return result
+
+def hill_cipher(message,key):
+  message=message.replace(" ","").upper()
+  if len(message)%2!=0:
+    message+="X"
+  key_matrix=get_key_matrix(key)
+  message_vector=[[ord(char)-65] for char in message]
+  chunks = [message_vector[i:i+len(key_matrix)] for i in range(0, len(message_vector), len(key_matrix))]
+  ciphertext = ""
+  for chunk in chunks:
+    result_vector = matrix_mult(key_matrix, chunk)
+    ciphertext += ''.join(chr(result[0] + 65) for result in result_vector)
+  return ciphertext
+key = list(map(int, input("Enter the key as a list of 4 numbers (2x2 matrix): ").split()))
+message = input("Enter the message to encrypt: ")
+ciphertext = hill_cipher(message, key)
+print("Ciphertext:", ciphertext)
+  
+ 
